0e8fef982abb18409cbcd31d27fa2622f9f05590
Colin Faivre <colin.faivre@gmail.com>
2024-02-25 22:22:48 +0100

    style: :lipstick: add bold style to skills titles

--- components/Skills.vue
+++ components/Skills.vue
@@ -3,10 +3,10 @@
     <hr>
 
     <ul>
-        <li>Vue3 <code>compositionAPI</code> <code>pinia</code></li>
-        <li>CSS <code>BEM</code> <code>SASS</code> <code>tailwind</code></li>
-        <li>Tools <code>vite</code> <code>bash</code> <code>gitlabCI</code></li>
-        <li>Code Quality <code>vitest</code> <code>cypress</code> <code>eslint</code> <code>prettier</code> <code>typescript</code></li>
-        <li>Languages <code>en</code> <code>fr</code></li>
+        <li><b>Nuxt3</b> <code>compositionAPI</code> <code>pinia</code></li>
+        <li><b>CSS</b> <code>BEM</code> <code>SASS</code> <code>tailwind</code></li>
+        <li><b>Tools</b> <code>vite</code> <code>bash</code> <code>gitlabCI</code></li>
+        <li><b>Code Quality</b> <code>vitest</code> <code>cypress</code> <code>eslint</code> <code>prettier</code> <code>typescript</code></li>
+        <li><b>Languages</b> <code>en</code> <code>fr</code></li>
     </ul>
 </template>
