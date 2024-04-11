f857740e65104595c0f7cb0efb2ed13205cb4dd0
Colin Faivre <colin.faivre@gmail.com>
2024-04-11 12:57:54 +0200

    fix: :bug: fix footer links

--- components/App/AppFooter.vue
+++ components/App/AppFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="p-5 bg-primary border-primary-light border-t-2">
         colin.faivre@gmail.com<br>
-        <a href="linkedin.com/in/colin-faivre-020498136">linkedin</a><br>
-        <a href="github.com/octodrome">github</a>
+        <a href="https://linkedin.com/in/colin-faivre-020498136">linkedin</a><br>
+        <a href="https://github.com/octodrome">github</a>
     </footer>
 </template>
